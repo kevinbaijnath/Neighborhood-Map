b6672d9395ff8594387cc40d997aa8a99c09036d
Kevin Baijnath <kevinbaijnath@gmail.com>
2017-02-10 11:56:36 -0500

    renamed class

---
+++ src/classes/restaurant.js
@@ -0,0 +1,21 @@
+import ko from 'knockout';
+
+/** Class representing a Restauraunt */
+class Restaurant {
+  /**
+   * Creates a new Restauraunt
+   * @constructor
+   * @param {string} name - Name of the Restauraunt
+   * @param {string} address - Address of the Restauraunt
+  */
+  constructor(name, address){
+    this.name = ko.observable(name);
+    this.address = ko.observable(address);
+    this.marker = ko.observable();
+    this.yelp_url = ko.observable();
+    this.yelp_img_url = ko.observable();
+    this.flickr_images = ko.observableArray();
+  }
+}
+
+export default Restaurant;
