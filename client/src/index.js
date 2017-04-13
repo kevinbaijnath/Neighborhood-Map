9dd875b39c522167f0de7db1b4609c13e8d72cf9
Kevin Baijnath <kevinbaijnath@gmail.com>
2017-04-13 00:17:55 -0400

    Add redux imports and store to index file

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
+import rootReducer from './reducers/reducers';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+let store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(ReduxThunk))
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
