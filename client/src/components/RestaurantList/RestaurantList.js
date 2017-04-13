77d00d0aab53d2fab79f7db4e06d2767be1a5047
Kevin Baijnath <kevinbaijnath@gmail.com>
2017-04-13 00:59:34 -0400

    Convert class based component to stateless functional component

--- client/src/components/RestaurantList/RestaurantList.js
+++ client/src/components/RestaurantList/RestaurantList.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './RestaurantList.css';
 
-class RestaurantList extends Component {
-    render(){
-        return (
-            <ul className="list-group">
-                {
-                 this.props.restaurants.map((restaurant, index) => {
-                    return (<li className={this.props.activeRestaurantIndex === index ? "list-group-item active" : "list-group-item" }
-                                key={restaurant.id} 
-                                onClick={() => {this.props.setActiveRestaurant(index)}}>
-                            {restaurant.name}</li>);
-                 })
-                }
-            </ul>
-        )
-    }
+const RestaurantList = (props) => {
+    return (
+        <ul className="list-group">
+            {
+                props.restaurants.map((restaurant, index) => {
+                return (<li className={props.activeRestaurantIndex === index ? "list-group-item active" : "list-group-item" }
+                            key={restaurant.id} 
+                            onClick={() => { props.updateActiveRestaurant(index)}}>
+                        {restaurant.name}</li>);
+                })
+            }
+        </ul>
+    )
 }
 
-export default RestaurantList
+export default RestaurantList;
